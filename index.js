fa55f5e7daa885ca985759906bd194cdeefe847c
MosesBonilla <bonillamoses@gmail.com>
2023-02-15 19:44:31 -0500

    closing for the day

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 //API: Application Programming Interface
-let squares = document.getElementById('square');
-console.log(squares);
+let squares = document.getElementById('square')
+console.log(squares)
 
 const board= document.getElementById('board')
 console.log(board)
+
+
+//this is called an arrow function
+let handleClick = () =>{
+    //this is where commands will sit
+    console.log("clicked!")
+}
+
+// alt method, this is an older, yet still valid method of setting variables
+// function handleClick(){
+//     //this is where commands go
+//     console.log('clicked')
+// }
+
+board.addEventListener('click', handleClick)
+
